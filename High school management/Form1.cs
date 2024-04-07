9017f1a9548c9b7ba9f8e03bab5e6f9ba2be07fe
TheCityzens <112615771+TheCityzens@users.noreply.github.com>
2024-04-07 18:49:28 +0700

    Push Thử
    okok

--- High school management/Form1.cs
+++ High school management/Form1.cs
@@ -10,8 +10,13 @@ using System.Windows.Forms;
 
 namespace High_school_management
 {
+    // Push thử
     public partial class Login : Form
     {
+        string username_student = "student";
+        string password_student = "123";
+        string username_teacher = "teacher";
+        string password_teacher = "1234";
         public Login()
         {
             InitializeComponent();
@@ -22,14 +27,37 @@ namespace High_school_management
 
         }
 
+        private void Close_Form(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+
         private void button_login_Click(object sender, EventArgs e)
         {
             string username, user_password;
-            username = txt_username.Text; 
+            username = txt_username.Text;
             user_password = txt_password.Text;
             try
             {
-                String querry = "select * from Table where Username = '"+txt_username+"' and Password = '"+txt_password+"'";
+                if (username == username_student)
+                {
+                    Form frmView = new Menu();
+                    frmView.Show();
+                    frmView.FormClosed += new FormClosedEventHandler(Close_Form);
+                    this.Hide();
+                }
+                else
+                if (username == username_teacher)
+                {
+                    Form frmView = new Menu();
+                    frmView.Show();
+                    frmView.FormClosed += new FormClosedEventHandler(Close_Form);
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Login Fault");
+                }
             }
             catch
             {
@@ -37,7 +65,7 @@ namespace High_school_management
             }
             finally
             {
-                
+
             }
         }
     }
--- High school management/Menu.Designer.cs
+++ High school management/Menu.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ViewScore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ViewScore
+            // 
+            this.ViewScore.Location = new System.Drawing.Point(525, 311);
+            this.ViewScore.Name = "ViewScore";
+            this.ViewScore.Size = new System.Drawing.Size(75, 23);
+            this.ViewScore.TabIndex = 0;
+            this.ViewScore.Text = "View Score";
+            this.ViewScore.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ViewScore);
+            this.Name = "Menu";
             this.Text = "Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ViewScore;
     }
 }
