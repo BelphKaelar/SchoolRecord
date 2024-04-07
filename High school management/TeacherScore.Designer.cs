df3b06f9a808c2e3c90458b8459663a11b97eb7d
Belphegor <147570771+BelphKaelar@users.noreply.github.com>
2024-04-08 00:16:08 +0700

    M.Update #1 Form Bảng điểm cho GV và HS chưa code

---
+++ High school management/TeacherScore.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace High_school_management
+{
+    partial class TeacherScore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TeacherScore));
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DSHS_Lb = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.Lich_Lb = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.TenHS_Tbox = new System.Windows.Forms.TextBox();
+            this.ApGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.DSDiem_Lb = new System.Windows.Forms.Label();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TenGV_Tbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Lop_Cbox = new System.Windows.Forms.ComboBox();
+            this.DiemMieng_Tbox = new System.Windows.Forms.TextBox();
+            this.Diem15p_Tbox = new System.Windows.Forms.TextBox();
+            this.Diem1Tiet_Tbox = new System.Windows.Forms.TextBox();
+            this.DiemGK_Tbox = new System.Windows.Forms.TextBox();
+            this.DiemCK_Tbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ApGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label6.Name = "label6";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Name = "label1";
+            // 
+            // DSHS_Lb
+            // 
+            resources.ApplyResources(this.DSHS_Lb, "DSHS_Lb");
+            this.DSHS_Lb.ForeColor = System.Drawing.Color.Black;
+            this.DSHS_Lb.Name = "DSHS_Lb";
+            this.DSHS_Lb.Click += new System.EventHandler(this.DSHS_Lb_Click);
+            // 
+            // pictureBox4
+            // 
+            resources.ApplyResources(this.pictureBox4, "pictureBox4");
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.TabStop = false;
+            // 
+            // Lich_Lb
+            // 
+            resources.ApplyResources(this.Lich_Lb, "Lich_Lb");
+            this.Lich_Lb.ForeColor = System.Drawing.Color.Black;
+            this.Lich_Lb.Name = "Lich_Lb";
+            this.Lich_Lb.Click += new System.EventHandler(this.Lich_Lb_Click);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.Color.Firebrick;
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox7
+            // 
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.TabStop = false;
+            // 
+            // TenHS_Tbox
+            // 
+            resources.ApplyResources(this.TenHS_Tbox, "TenHS_Tbox");
+            this.TenHS_Tbox.Name = "TenHS_Tbox";
+            // 
+            // ApGridView1
+            // 
+            resources.ApplyResources(this.ApGridView1, "ApGridView1");
+            this.ApGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ApGridView1.Name = "ApGridView1";
+            this.ApGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            // 
+            // DSDiem_Lb
+            // 
+            resources.ApplyResources(this.DSDiem_Lb, "DSDiem_Lb");
+            this.DSDiem_Lb.ForeColor = System.Drawing.Color.Black;
+            this.DSDiem_Lb.Name = "DSDiem_Lb";
+            this.DSDiem_Lb.Click += new System.EventHandler(this.DSDiem_Lb_Click);
+            // 
+            // CancelBtn
+            // 
+            resources.ApplyResources(this.CancelBtn, "CancelBtn");
+            this.CancelBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.DSHS_Lb);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.Lich_Lb);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.DSDiem_Lb);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Name = "panel1";
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // AddBtn
+            // 
+            resources.ApplyResources(this.AddBtn, "AddBtn");
+            this.AddBtn.BackColor = System.Drawing.Color.Black;
+            this.AddBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.UseVisualStyleBackColor = false;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // EditBtn
+            // 
+            resources.ApplyResources(this.EditBtn, "EditBtn");
+            this.EditBtn.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.DiemCK_Tbox);
+            this.panel2.Controls.Add(this.DiemGK_Tbox);
+            this.panel2.Controls.Add(this.Diem1Tiet_Tbox);
+            this.panel2.Controls.Add(this.Diem15p_Tbox);
+            this.panel2.Controls.Add(this.DiemMieng_Tbox);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.TenGV_Tbox);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.Lop_Cbox);
+            this.panel2.Controls.Add(this.TenHS_Tbox);
+            this.panel2.Controls.Add(this.ApGridView1);
+            this.panel2.Controls.Add(this.EditBtn);
+            this.panel2.Controls.Add(this.AddBtn);
+            this.panel2.Controls.Add(this.CancelBtn);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Name = "panel2";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label4.Name = "label4";
+            // 
+            // TenGV_Tbox
+            // 
+            resources.ApplyResources(this.TenGV_Tbox, "TenGV_Tbox");
+            this.TenGV_Tbox.Name = "TenGV_Tbox";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Name = "label2";
+            // 
+            // Lop_Cbox
+            // 
+            resources.ApplyResources(this.Lop_Cbox, "Lop_Cbox");
+            this.Lop_Cbox.FormattingEnabled = true;
+            this.Lop_Cbox.Items.AddRange(new object[] {
+            resources.GetString("Lop_Cbox.Items"),
+            resources.GetString("Lop_Cbox.Items1")});
+            this.Lop_Cbox.Name = "Lop_Cbox";
+            // 
+            // DiemMieng_Tbox
+            // 
+            resources.ApplyResources(this.DiemMieng_Tbox, "DiemMieng_Tbox");
+            this.DiemMieng_Tbox.Name = "DiemMieng_Tbox";
+            // 
+            // Diem15p_Tbox
+            // 
+            resources.ApplyResources(this.Diem15p_Tbox, "Diem15p_Tbox");
+            this.Diem15p_Tbox.Name = "Diem15p_Tbox";
+            // 
+            // Diem1Tiet_Tbox
+            // 
+            resources.ApplyResources(this.Diem1Tiet_Tbox, "Diem1Tiet_Tbox");
+            this.Diem1Tiet_Tbox.Name = "Diem1Tiet_Tbox";
+            // 
+            // DiemGK_Tbox
+            // 
+            resources.ApplyResources(this.DiemGK_Tbox, "DiemGK_Tbox");
+            this.DiemGK_Tbox.Name = "DiemGK_Tbox";
+            // 
+            // DiemCK_Tbox
+            // 
+            resources.ApplyResources(this.DiemCK_Tbox, "DiemCK_Tbox");
+            this.DiemCK_Tbox.Name = "DiemCK_Tbox";
+            // 
+            // TeacherScore
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TeacherScore";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ApGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DSHS_Lb;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label Lich_Lb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.TextBox TenHS_Tbox;
+        private System.Windows.Forms.DataGridView ApGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label DSDiem_Lb;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox Lop_Cbox;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TenGV_Tbox;
+        private System.Windows.Forms.TextBox DiemCK_Tbox;
+        private System.Windows.Forms.TextBox DiemGK_Tbox;
+        private System.Windows.Forms.TextBox Diem1Tiet_Tbox;
+        private System.Windows.Forms.TextBox Diem15p_Tbox;
+        private System.Windows.Forms.TextBox DiemMieng_Tbox;
+    }
+}
---
+++ High school management/TeacherScore.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace High_school_management
+{
+    public partial class TeacherScore : Form
+    {
+        public TeacherScore()
+        {
+            InitializeComponent();
+            if (Login.Role == "Teacher")
+            {
+                DSDiem_Lb.Enabled = true;
+                Lich_Lb.Enabled = false;
+                DSHS_Lb.Enabled = true;
+                Appoint_Lb.Enabled = true;
+            }
+            GetLopID(); 
+            DisplayStudentScore(); ApGridView1.ClearSelection();
+        }
+
+        int key = 0;
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\ADMINS\Source\Repos\High-School-Management1\High school management\SchoolRecord.mdf"";Integrated Security=True");
+
+        private void DisplayStudentScore()
+        {
+            string Query = "Select * from StudentScoreTb";
+            SqlDataAdapter SDA = new SqlDataAdapter(Query, Con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(SDA);
+            var ds = new DataSet();
+            SDA.Fill(ds);
+            ApGridView1.DataSource = ds.Tables[0];
+            Con.Close();
+        }
+
+        private void Clear()
+        {
+            Lop_Cbox.SelectedIndex = 0;
+            TenGV_Tbox.Text = string.Empty;
+            TenHS_Tbox.Text = string.Empty;
+            key = 0;
+        }
+
+        private void GetLopID()
+        {
+            Con.Open();
+            SqlCommand cmd = new SqlCommand("Select MaLop from LopHoc", Con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("DocID", typeof(int));
+            dt.Load(dr);
+            Lop_Cbox.ValueMember = "MaLop";
+            Lop_Cbox.DataSource = dt;
+            Con.Close();
+        }
+
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DSDiem_Lb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lich_Lb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DSHS_Lb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Login obj = new Login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void ApGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (ApGridView1.SelectedRows.Count > 0)
+            {
+                DiemMieng_Tbox.Text = ApGridView1.SelectedRows[0].Cells[8].Value.ToString();
+                Diem15p_Tbox.Text = ApGridView1.SelectedRows[0].Cells[9].Value.ToString();
+                Diem1Tiet_Tbox.Text = ApGridView1.SelectedRows[0].Cells[10].Value.ToString();
+                DiemGK_Tbox.Text = ApGridView1.SelectedRows[0].Cells[11].Value.ToString();
+                DiemCK_Tbox.Text = ApGridView1.SelectedRows[0].Cells[12].Value.ToString();
+                if (TenHS_Tbox.Text == string.Empty || TenGV_Tbox.Text == string.Empty) { key = 0; }
+                else { key = Convert.ToInt32(ApGridView1.SelectedRows[0].Cells[0].Value.ToString()); }
+            }
+            else { MessageBox.Show("Hãy chọn học sinh muốn thay đổi."); }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
